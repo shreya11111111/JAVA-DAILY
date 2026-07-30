dc7d5d999a761e5242a83382a1fa581c1fc21d1e
Shreya Panwar <shreyapanwar051@gmail.com>
2026-07-30 12:58:52 +0530

    packages

---
+++ Twenty9july26/Controller.java
@@ -0,0 +1,18 @@
+package Twenty9july26;
+public class Controller {
+  public static void  main(String[] args){
+
+    Demo d = new Demo();
+    System.out.println(d);
+
+
+    Demo.Task t = new Demo.Task();
+    System.out.println(t);
+
+
+    Demo.Test te = new Demo.Test();
+    System.out.println(te);
+
+  }
+  
+}
---
+++ Twenty9july26/Demo.java
@@ -0,0 +1,10 @@
+package Twenty9july29;
+
+public class Demo{
+  static class Task{
+
+  }
+  class Test{
+
+  }
+}
